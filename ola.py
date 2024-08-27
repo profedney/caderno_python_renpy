731738427c8caee136cc050c6be26be5832153c2
prof Edney <111137546+profedney@users.noreply.github.com>
2024-08-27 12:22:56 -0300

    Add files via upload

--- ola.py
+++ ola.py
@@ -1,8 +1,12 @@
-ola="Olá mundão pra lá de bão"
-print(ola)
-
-a=8
-b=12
+a=12
+b=3
 somar=a+b
+sub=a-b
+mult=a*b
+div=a/b
 print("A Soma é:",somar)
+print("A Subtração", sub)
+print("A Multiplicação é: ", mult)
+print("A Divisão é:", div)
+
 
