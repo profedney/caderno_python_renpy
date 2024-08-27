e1ccf3baa631def9a6f576eb1f69a067c8eba8e8
prof Edney <111137546+profedney@users.noreply.github.com>
2024-08-27 12:24:58 -0300

    Update ola.py

--- ola.py
+++ ola.py
@@ -1,12 +1,13 @@
-a=12
-b=3
-somar=a+b
-sub=a-b
-mult=a*b
-div=a/b
-print("A Soma é:",somar)
-print("A Subtração", sub)
-print("A Multiplicação é: ", mult)
+a = 12  # Atribui o valor 12 à variável 'a'
+b = 3   # Atribui o valor 3 à variável 'b'
+
+somar = a + b  # Soma 'a' e 'b' e armazena o resultado em 'somar'
+sub = a - b    # Subtrai 'b' de 'a' e armazena o resultado em 'sub'
+mult = a * b   # Multiplica 'a' por 'b' e armazena o resultado em 'mult'
+div = a / b    # Divide 'a' por 'b' e armazena o resultado em 'div'
+
+# Exibe os resultados das operações
+print("A Soma é:", somar)
+print("A Subtração é:", sub)
+print("A Multiplicação é:", mult)
 print("A Divisão é:", div)
-
-
